4146cbdb04323034742522f6a290966dd63a0502
Cristian04-gif <cadrianocondor34@gmail.com>
2026-07-12 19:40:09 -0500

    imagenes catalog

--- catalog_service/src/main/java/com/home_fixer_hub/catalog_service/Domain/Service/CloudinaryService.java
+++ catalog_service/src/main/java/com/home_fixer_hub/catalog_service/Domain/Service/CloudinaryService.java
@@ -3,6 +3,7 @@ package com.home_fixer_hub.catalog_service.Domain.Service;
 import java.io.SequenceInputStream;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 
@@ -17,11 +18,14 @@ public class CloudinaryService {
 
     private final Cloudinary cloudinary;
 
-    public CloudinaryService() {
+    public CloudinaryService(
+            @Value("${cloudinary.cloud_name}") String cloudName,
+            @Value("${cloudinary.api_key}") String apiKey,
+            @Value("${cloudinary.api_secret}") String apiSecret) {
         this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dep5zhuay",
-                "api_key", "613478824826563",
-                "api_secret", "TnM60owVajuJDqAInSYvD_3u7_Y",
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
                 "secure", true));
     }
 
