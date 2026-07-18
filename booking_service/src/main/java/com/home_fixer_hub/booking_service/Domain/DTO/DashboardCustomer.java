8e66eb49340b5317503fc4671509eac8e538ff25
Cristian04-gif <cadrianocondor34@gmail.com>
2026-07-18 13:13:16 -0500

    comentarios

--- booking_service/src/main/java/com/home_fixer_hub/booking_service/Domain/DTO/DashboardCustomer.java
+++ booking_service/src/main/java/com/home_fixer_hub/booking_service/Domain/DTO/DashboardCustomer.java
@@ -1,5 +1,14 @@
 package com.home_fixer_hub.booking_service.Domain.DTO;
 
-public class DashboardCustomer {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class DashboardCustomer {
+    private Long pendingQueries;
 }
